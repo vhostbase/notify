bf7952085b0fbc0abc0dc1b1bd5a8b927f615a51
vhostbase <vhostbase@gmail.com>
2020-04-04 12:15:38 +0530

    upd

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
-})
+})*/
 
 var server = app.listen(8080, function () {
    var host = server.address().address
