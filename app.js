04d58a8076a6846b22b7f1fe34795bb56fef0cbe
vhostbase <vhostbase@gmail.com>
2020-04-04 12:11:44 +0530

    upd

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
 })
 
-var server = app.listen(80, function () {
+var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("App listening at http://%s:%s", host, port)
